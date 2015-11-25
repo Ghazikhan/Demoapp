de4284b2e71c03e02679c0777817f43cc60cc7a6
Ghazikhan <ghazi545@gmail.com>
2015-11-25 21:01:23 +0500

    adding user information section

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151124112911) do
+ActiveRecord::Schema.define(version: 20151125091134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "userinfos", force: :cascade do |t|
+    t.integer  "user_id"
+    t.date     "date_of_birth"
+    t.string   "gender"
+    t.string   "mobile"
+    t.text     "address"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "userinfos", ["user_id"], name: "index_userinfos_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -40,4 +52,5 @@ ActiveRecord::Schema.define(version: 20151124112911) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "userinfos", "users"
 end
